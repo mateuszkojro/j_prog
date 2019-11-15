89b727beb11fd0f3c07f32bfbf6e74749ed97f90
mateuszkojro <mateusz.kojro@unilodz.eu>
2019-11-15 21:48:24 +0100

    mozliwosc miezenia czasu , ustawienia vscoda, funckcja do wypisywania na ekran prosta

---
+++ biblioteka/build/CMakeFiles/biblioteka.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/biblioteka.dir/src/main.cpp.o"
+  "CMakeFiles/biblioteka.dir/src/simple.cpp.o"
+  "biblioteka.pdb"
+  "biblioteka"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/biblioteka.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- biblioteka/src/main.cpp
+++ biblioteka/src/main.cpp
@@ -1,9 +1,6 @@
 
 
-
-//jezeli wszytsko jest w jednym pliku to sie kompiluje bez problemu 
-
-
+// jezeli wszytsko jest w jednym pliku to sie kompiluje bez problemu
 
 #include "simple.h"
 #include <cstdlib>
@@ -14,14 +11,15 @@ int main() {
   simple::init();
   int a = 0;
   int b = 10;
-  const int size = 100;
+  const int size = 19999;
   int tab[size];
 
   int d2[3][3] = {};
 
-  int x = simple::random(3,5);
-  std::cout << x;
-  simple::sort(tab, size);
-  simple::list_show(tab, size);
-  simple::arr_show(d2, 3.3);
+  for (int i = 0; i < size; i++) {
+    tab[i] = simple::random(0, 10000);
+  }
+  {measure_time timing;simple::sort(tab, size);}
+  //simple::list_show(tab, size);
+  // simple::arr_show(d2, 3.3);
 }
